0689bff2078e0efc62b11a77aec80ccddb852032
Saves <martin.l74123@gmail.com>
2026-08-06 17:03:13 +0200

    feat: implement Spotify PKCE auth flow with token exchange

--- src/routes/Callback.tsx
+++ src/routes/Callback.tsx
@@ -1,6 +1,54 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
+import { useSearchParams } from "react-router-dom"
+
 
 function Callback() {
+    const hasRun = useRef(false);
+    const [searchParams] = useSearchParams();
+    const code = searchParams.get("code");
+
+    console.log(code)
+    console.log("log po code")
+
+  
+
+
+    async function exchangeCodeForToken() {
+        const verifier = localStorage.getItem('code_verifier')
+
+        if(verifier === null || code === null) {
+            return console.error("Error with null type")
+        }
+
+         const params = new URLSearchParams({
+              grant_type: "authorization_code",
+              client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+              redirect_uri: "http://127.0.0.1:5173/callback",
+              code_verifier:verifier,
+              code
+          });
+
+       const response = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: params
+        });
+
+        const data = await response.json();
+        console.log("Data: ")
+        console.log(data)
+    }
+
+    useEffect(() => {
+        if(hasRun.current) return;
+
+        hasRun.current = true;
+        exchangeCodeForToken()
+    }, [])
+
+
   return (
     <div>Callback</div>
   )
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useState } from "react";
 import { generateCodeVerifier, generateCodeChallenge } from "../auth/pkce"
 
 function Login() {
