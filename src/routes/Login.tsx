50e2ff9a84575894d4a2c195a8fd8c46cc01d772
Saves <martin.l74123@gmail.com>
2026-08-07 14:02:33 +0200

    feat: fetch user playlists with access token

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -29,6 +29,25 @@ function Login() {
         window.location.href = authUrl
         console.log("test-----")
     }
+
+    async function fetchPlaylists() {
+      const token = localStorage.getItem('access_token');
+
+      if(token === null) {
+        return console.error("Token null value error")
+      }
+
+      const response = await fetch("https://api.spotify.com/v1/me/playlists", {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
+
+      const data = await response.json();
+      console.log("Data: ")
+      console.log(data)
+    }
   
 
 
@@ -36,7 +55,10 @@ function Login() {
 
 
   return (
+    <>
     <button className="p-1 m-4 border rounded-md" onClick={() => handleLogin()}>Login</button>
+    <button onClick={() => fetchPlaylists()}>Show Playlist</button>
+    </>
   )
 }
 
